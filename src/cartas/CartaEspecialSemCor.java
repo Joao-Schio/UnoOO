779a1a81b02596c03546268c0e3a9f220c6c1e9b
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2022-10-03 14:24:39 -0400

    Update CartaEspecialSemCor.java

--- src/cartas/CartaEspecialSemCor.java
+++ src/cartas/CartaEspecialSemCor.java
@@ -3,25 +3,50 @@ package cartas;
 public class CartaEspecialSemCor extends CartaComAcao {
 
 	private String acao;
+	/**
+	 * @author joao-schio
+	 * guarda a acao da carta
+	 */
 
-
-	
-	public CartaEspecialSemCor(){
-
+	public CartaEspecialSemCor () {
+	/**
+	 * @author joao-schio
+	 * cria o objeto
+	 * 
+	 */
 	}
 	public CartaEspecialSemCor (String ac){
 		this.acao = ac;
-	}
-	public String getAcao(){
-		return this.acao;
-	}
-	public void setAcao(string ac){
-		this.acao = ac;
+	/**
+	 * @author joao-schio
+	 * Cria e define a acao
+	 */
 	}
 
-	@Override
-	public final Cor getCor() throws CartaSemCor{
-		return "Essa carta nao possui cor";
+	public void setAcao(String ac){
+		this.acao = ac;
+	/**
+	 * @author joao-schio
+	 * seta a acao da carta 
+	 */
+	}
+
+	public String getAcao(){
+		return this.acao;
+	/**
+	 * @author joao-schio
+	 * devolve a acao da carta 
+	 */
 	}
 	
+	public final Cor getCor(){
+
+
+	/**
+	 * @author joao-schio
+	 * CartaEspecialSemCor nao possui cor
+	 * ainda eh preciso criar uma exception para esse caso 
+	 */
+
+	}
 }
